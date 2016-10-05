4915844d74b7d282bc36356fb36715dbdec19113
Eilin Paola Luna Moreno <eipalumor12@hotmail.com>
2016-10-05 10:06:13 -0500

    Delete Entero.py

--- Entero.py
+++
@@ -1,8 +0,0 @@
-# CLASS ENTERO
-class Entero:
-    def __init__(self, ent):
-        self.entero = ent # Expresion entera (str)
-        self.e = int(self.entero) # Entero (int)
-
-    def __str__(self):
-        return str(self.entero)
